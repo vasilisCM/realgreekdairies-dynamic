7c965dfa410851af90d118994ac7748fe2882c62
vasilisCM <vpafiliaris@conceptmaniax.com>
2023-12-06 17:21:59 +0200

    404

--- 404.php
+++ 404.php
@@ -1,33 +1,37 @@
-<?php get_header(); ?> <!-- Header  -->
-    
-
-    <!-- Main  -->
-    <main class="main">
-      <section class="hero">
-        <div class="hero__container">
-          <div class="hero__background" style="
-              background-image: url(https://pbs.twimg.com/media/CCNMHroUMAACbwd.jpg);
-            "></div>
-
-          <div class="curve">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" class="shape-fill"></path>
-            </svg>
-          </div>
-        </div>
-
-        <div class="card-text card-text--center hero__card boxed centered">
-          <h1 class="heading card-text__heading">Σφάλμα 404</h1>
-          <p class="card-text__text">Η σελίδα δε βρέθηκε.</p>
-        </div>
-
-        <div class="hero__shape-divider">
-          <img data-uploads="home-milk-shape-divider.png" src=" <?php echo 
-    str_replace( wp_upload_dir()['basedir'], 
-    wp_upload_dir()['baseurl'],
-    wp_upload_dir()['basedir'] . wp_upload_dir()['subdir'] . '/' . 'home-milk-shape-divider.png' ); ?>" alt="" class="milestones__shape-divider-img">
-        </div>
-      </section>
-    </main>
-
+<?php get_header(); ?> <!-- Header  -->
+
+    
+
+
+
+
+        <!-- Main  -->
+        <main class="main">
+      <section class="hero error-404">
+        <div class="hero__container">
+          <div class="hero__background">
+            <div class="error-404__container boxed centered">
+              <div class="error-404__img-container">
+                <img class="centered" src="<?php echo get_template_directory_uri() . '/./assets/img/guru-goat.png'; ?>" alt="" />
+              </div>
+              <div class="error-404__text-container">
+                <h2 class="error-404__heading">404</h2>
+                <p class="heading-medium-large error-404__text">
+                  Η σελίδα δεν βρέθηκε.
+                </p>
+              </div>
+              <a href="/"
+                ><button class="button">
+                  <span>Αρχική</span>
+                </button></a
+              >
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+
+
+
     <!-- Footer  --> <?php get_footer(); ?>
